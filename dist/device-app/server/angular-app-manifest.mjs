8ce494223e0cf8daa7ac3e597881bac3d401848f
William <mmupfururirwa@smartviewtechnology.co.za>
2025-03-17 19:34:19 +0200

    cloud build

--- dist/device-app/server/angular-app-manifest.mjs
+++ dist/device-app/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 493, hash: '3b2c7d9739b1bafcfdebaba82af7e83d6d561759375c02b1c2f4a015ea06fb74', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1006, hash: 'ec89812b88726a3cad8cca5830f51fb22fd556142419797e65207753b8b84ea9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 2297, hash: '53eeffac8699b7612830cfd8edc9b4d535eb47a8f636be4bdda231a528476a81', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 493, hash: '30a9450e19cc17947ba4a4675ef66aa7195bed8fac3c73f331e9dd80848bbe9a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1006, hash: 'a91c64528b4776c77a92f43442706e2873b641e310864614dd771a95d5074ca4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 2297, hash: '983d40fe557275e2a12186eb7e01d5d349ab5e868fedfe714818012293668874', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
