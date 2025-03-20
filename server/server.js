3d4572dde45ac49b9de227a589d22899f7a1ab49
William <mmupfururirwa@smartviewtechnology.co.za>
2025-03-20 10:29:08 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.post('/api/auth/register', async (req, res) => {
     res.json(response.data);
   } catch (error) {
     console.error('Proxy register error:', error.message);
-    console.error('Proxy register message:', error);
+    console.error('Proxy register message:', error.response.data);
     res.status(500).json({ message: 'Register failed', error: error.message });
   }
 });
